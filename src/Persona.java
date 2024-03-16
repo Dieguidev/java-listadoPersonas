601f10cc0f5985deb4435a0aa5fb1451713a24bc
Dieguidev <diegogaraycullas@gmail.com>
2024-03-16 07:16:49 -0500

    configurado el metodo toString

--- src/Persona.java
+++ src/Persona.java
@@ -18,4 +18,52 @@ public class Persona {
         this.email = email;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    //sobrescribiendo la clase toString() para imprimir los datos de la persona.
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Perez", "123456789", "juan@gmail.com");
+        System.out.println(persona1);
+    }
 }
