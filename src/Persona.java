fd45209d7d7272cab980713d33815a3f22a75e11
Dieguidev <diegogaraycullas@gmail.com>
2024-03-16 06:59:12 -0500

    first commit

---
+++ src/Persona.java
@@ -0,0 +1,21 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    //constructor vacio
+    public Persona() {
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    //constructor con argumentos
+    public Persona(String nombre, String telefono, String email) {
+        this();
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+}
